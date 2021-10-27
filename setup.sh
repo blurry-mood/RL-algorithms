68d7997f5eb8f26fc1f543587075781b146da0af
blurry-mood <assis.ayoub@gmail.com>
2021-10-27 03:02:59 +0300

    install `gym`, `nle` & `minihack`

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# Python and most build deps
+sudo apt-get install -y build-essential autoconf libtool pkg-config \
+    python3-dev python3-pip python3-numpy git flex bison libbz2-dev
+
+# recent cmake version
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+sudo apt-get update && apt-get --allow-unauthenticated install -y \
+    cmake \
+    kitware-archive-keyring
+
+python3 -m venv venv
+source venv/bin/activate
+pip install nle
+
+# install MiniHack
+pip install minihack
