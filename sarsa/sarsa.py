fix error in update formula for `sarsa` algorithm

sarsa/sarsa.py
@@ -15,36 +15,53 @@ class Sarsa:
         self.alpha = alpha
         self.gamma = gamma
         self.eps = eps
-        self.q = {}
-        self.prev_state = None
-        self.prev_action = None
 
-    def action_value(self, state, action):
+        self.q = {}
+        self.sar = []   # stands for state-action-reward
+
+    def _action_value(self, state, action):
         return self.q.get((state, action), 1e-3*np.random.randn())
 
     def _get_action(self, state, eps):
         if np.random.rand() < eps:
             return np.random.choice(self.actions)
-        qs = {(action, self.action_value(state=state, action=action)) for action in self.actions}
+        qs = {(action, self._action_value(state=state, action=action)) for action in self.actions}
         action = max(qs, key = lambda x: x[1])
         return action[0]
 
-    def update(self, state, action, reward):
-        q = self.action_value(state=self.prev_state, action=self.prev_action)
-        tmp = reward - q
-        tmp += self.gamma * self.action_value(state, action)
-        self.q[(self.prev_state, self.prev_action)] = q + self.alpha * tmp
-        self.prev_state = state
-        self.prev_action = action
+    def update(self, reward):
+        """ Update state-action value of previous (state, action).
+        
+        - `reward`: reward received upon the transaction to `next_state` from previous state.
+        """
+        self.sar[-1][-1] = reward
+        if len(self.sar) < 2:
+            return
 
-    def take_action(self, state):
-        action = self._get_action(state, self.eps)
+        q = self._action_value(state=self.sar[0][0], action=self.sar[0][1])
+        tmp = self.sar[0][2] - q
+        tmp += self.gamma * self._action_value(state=self.sar[1][0], action=self.sar[1][1])
+        self.q[(self.sar[0][0], self.sar[0][1])] = q + self.alpha * tmp
+
+        del self.sar[0]
+
+    def take_action(self, current_state):
+        """ Choose an eps-greedy action to be taken when current state is `current_state`. """
+        action = self._get_action(current_state, self.eps)
+        self.sar.append([current_state, action, 0])
         return action
 
     def save(self, path):
+        """ Load state-action value table in `path`.npy """
+
         np.save(join(_HERE, path + '.npy'), self.q)
 
     def load(self, path):
+        """ Load state-action value table.
+        
+        If it doesn't exist, use random q-value.
+        """
+
         try:
             self.q = np.load(join(_HERE, path + '.npy'), allow_pickle='TRUE').item()
         except:
@@ -54,8 +71,8 @@ class Sarsa:
 if __name__ == '__main__':
     ALPHA = 1e-1
     GAMMA = 9e-1
-    EPS = 1e-1
-    ITERS = 100
+    EPS = 5e-1
+    ITERS = 20
     
     MOVE_ACTIONS = list(range(len(nethack.CompassDirection)))
 
@@ -73,8 +90,7 @@ if __name__ == '__main__':
         observation_keys=STATE,
         max_episode_steps=100_000_000,
         obs_crop_h=5,
-        obs_crop_w=5,
-        
+        obs_crop_w=5
     )
 
     for i in range(ITERS):
@@ -89,7 +105,8 @@ if __name__ == '__main__':
             state, reward, done, info = env.step(action)
             state = tuple(map(tuple, state[STATE[0]]))
 
-            sarsa.update(state, action, reward)
+            sarsa.update(reward)
+
             arr = env.render('ansi')
             print(arr.replace(" ", "").replace("\n\n", ''))
             